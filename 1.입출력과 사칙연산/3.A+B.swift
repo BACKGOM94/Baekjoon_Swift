77d87258c76076371f44527e54d5a575f47f5492
BACKGOM94 <backgom94@gmail.com>
2022-09-15 05:23:03 +0900

    6번 완료

---
+++ 1.입출력과 사칙연산/3.A+B.swift
@@ -0,0 +1,12 @@
+//https://www.acmicpc.net/problem/1000
+
+// 옵셔널 업래핑 개념 공부할것
+
+import Foundation
+
+let num = readLine()!.split(separator: " ")
+
+let A = Int(num[0])!
+let B = Int(num[1])!
+let sum = A + B
+print(String(sum))
---
+++ 1.입출력과 사칙연산/5.A*B.swift
@@ -0,0 +1,12 @@
+//https://www.acmicpc.net/problem/10998
+
+import Foundation
+
+let num = readLine()!.split(separator: " ")
+
+let A = Int(num[0])!
+let B = Int(num[1])!
+
+let value = A * B
+
+print(value)
